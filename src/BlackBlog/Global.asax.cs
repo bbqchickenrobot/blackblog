7554197f95fdeb627108d15fe4a956eda3979035
Kestrel Blackmore <me@kestrel.com>
2012-12-03 16:58:28 +1030

    removed route from global.asax

--- src/BlackBlog/Global.asax.cs
+++ src/BlackBlog/Global.asax.cs
@@ -27,12 +27,6 @@ namespace BlackBlog
                 new { controller = "Home", action = "Index", id = UrlParameter.Optional } // Parameter defaults
             );
 
-            routes.MapRoute(
-                "Stub", // Route name
-                "{controller}/{action}/{id}", // URL with parameters
-                new { controller = "Home", action = "Index", id = UrlParameter.Optional } // Parameter defaults
-            );
-
         }
 
         protected void Application_Start()
